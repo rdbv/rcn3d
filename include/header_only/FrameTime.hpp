3323e29fb5aa488ace0138bfd328dfebeace6caa
rdbv <brgb321@gmail.com>
2016-06-24 15:32:46 +0200

    kurwa zjebalem cos

--- include/header_only/FrameTime.hpp
+++ include/header_only/FrameTime.hpp
@@ -17,16 +17,16 @@ namespace rcn3d {
 class FrameTime {
 public:
     FrameTime() {
-        m_frameTimes.fill(16.0f);
+        frameTimes.fill(16.0f);
     }
     ~FrameTime() {}
 
     void begin() {
-        m_startTicks = SDL_GetTicks();
+        startTicks = SDL_GetTicks();
     }
 
     void end() {
-        float frameTicks = SDL_GetTicks() - m_startTicks;
+        float frameTicks = SDL_GetTicks() - startTicks;
         if(frameTicks < 1000.0f / 60.0f) {
             SDL_Delay(static_cast<Uint32>((1000.0f / 60.0f) - frameTicks));
         }
@@ -34,16 +34,16 @@ public:
     }
 
     float getDeltaTime() {
-        return m_delta / 1000.0f;
+        return delta / 1000.0f;
     }
 
 private:
     void calculateFps() {
-        m_delta = SDL_GetTicks() - m_startTicks;
-        m_frameTimes[m_currentFrame % m_numberOfSamples] = m_delta;
-        m_currentFrame++;
+        delta = SDL_GetTicks() - startTicks;
+        frameTimes[currentFrame % numberOfSamples] = delta;
+        currentFrame++;
 
-        float avg = (std::accumulate(m_frameTimes.begin(), m_frameTimes.end(), 0.0f)) / (m_numberOfSamples);
+        float avg = (std::accumulate(frameTimes.begin(), frameTimes.end(), 0.0f)) / (numberOfSamples);
         static int c = 0;
         if(avg > 0 && c > 60) {
             std::cout << 1000.0f / avg << "\n";
@@ -52,12 +52,12 @@ private:
         c++;
     }
 
-    Uint32  m_startTicks;
-    Uint32  m_delta;
+    Uint32  startTicks;
+    Uint32  delta;
 
-    int     m_currentFrame = 0;
-    static const int   m_numberOfSamples = 10;
-    std::array <Uint32, m_numberOfSamples> m_frameTimes;
+    int     currentFrame = 0;
+    static const int   numberOfSamples = 10;
+    std::array <Uint32, numberOfSamples> frameTimes;
 };
 
 }
