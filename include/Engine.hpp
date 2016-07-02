91882d513f5979ea5585e5e05700f42ba3ebde04
Stanisław Koza <stasiukoza1234@gmail.com>
2016-07-02 15:44:16 +0200

    Revert "Engine.hpp includes refactor"
    This reverts commit 7247efb95f40d5a0b2d38ceefe83bee11838b95b.

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -1,18 +1,18 @@
 #ifndef ENGINE_HPP
 #define ENGINE_HPP
 
-#include "header_only/Math.hpp"
-#include "header_only/VertexArray.hpp"
-#include "header_only/VertexBuffer.hpp"
-#include "header_only/DebugCamera.hpp"
-#include "header_only/FrameTime.hpp"
-#include "header_only/Texture.hpp"
-#include "header_only/Timer.hpp"
+#include "../include/header_only/Math.hpp"
+#include "../include/header_only/VertexArray.hpp"
+#include "../include/header_only/VertexBuffer.hpp"
+#include "../include/header_only/DebugCamera.hpp"
+#include "../include/header_only/FrameTime.hpp"
+#include "../include/header_only/Texture.hpp"
+#include "../include/header_only/Timer.hpp"
 
-#include "SDL_Context.hpp"
-#include "ShaderProgram.hpp"
-#include "TextureLoader.hpp"
-#include "Renderer.hpp"
+#include "../include/SDL_Context.hpp"
+#include "../include/ShaderProgram.hpp"
+#include "../include/TextureLoader.hpp"
+#include "../include/Renderer.hpp"
 
 namespace rcn3d {
 
