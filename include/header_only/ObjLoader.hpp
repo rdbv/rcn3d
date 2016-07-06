13639a00f1e496e69f1cd5641518a3fee5ab60f6
Robert Lewicki <zguba44@gmail.com>
2016-07-06 15:44:28 +0200

    Loading .obj files

--- include/header_only/ObjLoader.hpp
+++ include/header_only/ObjLoader.hpp
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <cstring>
 #include <vector>
+#include <string>
+#include <fstream>
 #include <glm/glm.hpp>
 #include "VertexArray.hpp"
 #include "VertexBuffer.hpp"
@@ -18,13 +20,9 @@ namespace rcn3d {
 struct ObjectData {
 
     std::vector<glm::vec3> vx;
-    std::vector<glm::vec3> uv;
-    std::vector<glm::vec3> normals;
-    std::vector<glm::vec3> vx_ebo, uv_ebo, normals_ebo;
-
-    char name[BUFFER_LENGTH];
-    bool isUVIncluded;
-
+    std::vector<glm::vec2> uv;
+    std::vector<glm::vec3> vn;
+    std::vector <std::vector <glm::ivec3>> faces;
 };
 
 
@@ -32,91 +30,85 @@ class ObjLoader
 {
 public:
 
-    bool loadObj(const char* path, VertexArray& m_vao) {
-        
-        VertexBuffer m_vbo;
-
-        //m_vao.createVertexArrays(1);
-        //m_vbo.createVertexBuffers(2);
-
-        return false;
-    }
-
-    bool loadObjData(const char* path, ObjectData& dat) {
-        FILE* f = fopen(path, "r");
-        char line[BUFFER_LENGTH];
-        std::vector<glm::vec3>* data_ptr = nullptr;
-
-        if(!f) {
-            printf("Failed to load %s\n", path);
+    bool loadObjData(const std::string& filePath) {
+        // Check file extension
+        auto p = filePath.end();
+        if(!(*p == 'j' && *(p - 1) == 'b' && *(p - 2) == 'o')) {
+            printf("File has wrong extension!\n");
             return false;
         }
 
-        char fc, c1;
-        float x, y, z;
-
-        while(fgets(line, BUFFER_LENGTH, f) != NULL) {
-            fc = line[0];            
-            c1 = line[1];
-
-            if(fc == '#')
-                continue;
-            
-            if(fc == 's')
-                continue;
-
-            if(fc == 'o') {
-                sscanf(line+2, "%s", dat.name);
-            }
-
-            if(fc == 'v' && (c1 != 't' && c1 != 'n') ) {
-                sscanf(line+2, "%f %f %f\n", &x, &y, &z); 
-                dat.vx.push_back(glm::vec3(x, y, z));
-            }
-             
-            if(fc == 'v' && c1 == 't') {
-                sscanf(line+3, "%f %f %f", &x, &y, &z);
-                dat.uv.push_back(glm::vec3(x, y, z));
-            }
-
-            if(fc == 'v' && c1 == 'n') {
-                sscanf(line+3, "%f %f %f", &x, &y, &z);
-                dat.normals.push_back(glm::vec3(x, y, z));
-            }
-            
-            if(fc =='f') {
-                char* beg = line+2;
-                char* sls_beg  = strchr(beg, '/');
-                unsigned int data[9];
-
-                if(!sls_beg)
-                    return false;
-
-                dat.isUVIncluded = sls_beg[0] != sls_beg[1]; 
-                               
-                
-                                        
-                if(dat.isUVIncluded) {
-
-                sscanf(beg, "%d/%d/%d %d/%d/%d %d/%d/%d", &data[0], &data[1], &data[2], 
-                                                          &data[3], &data[4], &data[5], 
-                                                          &data[6], &data[7], &data[8]);
-
-                }
-                else {
-                    sscanf(beg, "%d//%d %d//%d %d//%d", &data[0], &data[2],
-                                                        &data[3], &data[5],
-                                                        &data[6], &data[8]);
-                                                        
-                }
-
-
-
-            }
-
+        // Open file and check if succeed
+        std::ifstream f(filePath);
+        if(f.fail()) {
+            printf("Could not load .obj file. Wrong file path\n");
+            return false;
         }
 
+        std::string buf;
+        ObjectData obj;
+        float x, y, z;
+        float u, v;
+        while(!f.eof()) {
+            f >> buf;
+            // Skip if current line is comment
+            if(buf[0] == '#') {
+                std::getline(f, buf);
+                continue;
+            }
+
+            if(buf == "usemtl") {
+                std::string materialPath;
+                f >> materialPath;
+                //TODO use material in given path
+            } else if(buf == "v") {
+                f >> x >> y >> z;
+                obj.vx.emplace_back(glm::vec3(x, y, z));
+            } else if(buf == "vt") {
+                f >> u >> v;
+                obj.uv.emplace_back(glm::vec2(u, v));
+            } else if(buf == "vn") {
+                f >> x >> y >> z;
+                obj.vn.emplace_back(glm::vec3(x, y, z));
+            } else if(buf == "f") {
+                std::getline(f, buf);
+                parseFaces(obj.faces, buf);
+            } else {
+                printf("Unknown sytanx in .obj file\n");
+            }
+        }
         return true;
+    }
+
+
+private:
+    void parseFaces(std::vector <std::vector<glm::ivec3>>& faces, std::string& s) {
+        std::vector <glm::ivec3> v;
+        v.reserve(3);
+        int x;
+
+        std::string t = "";
+        int c = 0;
+        std::size_t c2 = 0;
+        for(std::size_t i = 0; i < s.size(); i++) {
+            if(s[i] != '/') {
+                t += s[i];
+            } else {
+                x = std::atoi(t.c_str());
+                if(c == 0) {
+                    v[c2].x = x;
+                } else if(c == 1) {
+                    v[c2].y = x;
+                } else if(c == 2) {
+                    v[c2].z = x;
+                    c2++;
+                    c = -1;
+                }
+                c++;
+                t = "";
+            }
+        }
+        faces.push_back(v);
     }
 
 };
